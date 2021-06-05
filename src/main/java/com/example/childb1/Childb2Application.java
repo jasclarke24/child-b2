55372ed16ddfb6e2496f9526077a3b8436b3996a
Jason Clarke <Jason.M.Clarke@ngc.com>
2021-06-05 18:36:31 -0400

    b2

---
+++ src/main/java/com/example/childb1/Childb2Application.java
@@ -0,0 +1,13 @@
+package com.example.childa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Childb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Childb2Application.class, args);
+	}
+
+}
